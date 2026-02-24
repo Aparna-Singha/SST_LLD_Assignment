667ad593b5499f7f65a340d5c8feb75ecf33ce2b
Aparna Singha <aparna.24bcs10353@sst.scaler.com>
2026-02-25 03:31:50 +0530

    Ex10 completed

---
+++ SOLID/Ex10/src/PaymentGateway.java
@@ -0,0 +1,9 @@
+public class PaymentGateway implements IPaymentGateway{
+
+    @Override
+    public String charge(String studentId, double amount) {
+        // fake deterministic txn
+        return "TXN-9001";
+    }
+    
+}
